053ffff73b9bf39f7e41689f813e1edd8f2cfb16
Saurabh Mathur <saurabhmathur96@gmail.com>
2014-10-13 21:39:43 +0530

    Create scrape-cbse.py

---
+++ scrape-cbse.py
@@ -0,0 +1,38 @@
+import sys
+from mechanize import Browser
+import sys
+from bs4 import BeautifulSoup
+
+def main(a1,a2,outfile):
+	data = []
+	b=Browser()
+	for rn in range(a1,a2+1):
+
+		b.open("http://cbseresults.nic.in/class12/cbse122014_total.htm")
+		b.select_form("FrontPage_Form1")
+		b["regno"]=str(rn)
+		resp=b.submit()
+		cont=resp.read()
+		soup=BeautifulSoup(cont);
+		soup.prettify("utf-8")
+		cont=soup.get_text();
+		cont=cont[cont.index("Roll"):cont.index("Check")]
+		cont=cont.encode("UTF-8").strip() 
+
+		data.append(cont)
+	
+	with open(outfile,'w') as f:
+		f.write('\n ----------------------------------------- \n'.join(data))
+
+if __name__ == "__main__":
+	arglen = len(sys.argv)
+	if arglen < 3 or arglen > 4:
+		print "error : usage scrape-cbse [from-roll-nos] [to-roll-nos] [outfile(optional)]"
+	else :
+		a1 = int(sys.argv[1])
+		a2 = int(sys.argv[2])
+		if arglen == 4 : 
+			outfile =  sys.argv[3]
+		elif arglen == 3:
+			outfile = sys.stdout;
+		main(a1,a2,outfile)
