0a99a15f5f6b8f1cb9eceda2a358210e71719109
Saurabh Mathur <saurabhmathur96@gmail.com>
2015-01-05 18:29:42 +0530

    Update scrape-cbse.py

--- scrape-cbse.py
+++ scrape-cbse.py
@@ -1,41 +1,50 @@
-import sys
+"""scrape-cbse.py : gets the cbse results by scraping the website
+
+"""
+
+from sys import stdout
 from mechanize import Browser
-import sys
 from bs4 import BeautifulSoup
+from argparse import ArgumentParser
 
-def main(a1,a2):
-	data = []
-	b=Browser()
-	for rn in range(a1,a2+1):
+USAGE = 'usage : python scrape-cbse.py \
+        [from-roll-num] \
+        [to-roll-num(optional)] \
+        [outfile(optional)]'
+URL = 'http://cbseresults.nic.in/class12/cbse122014_total.htm'
+FORM = 'FrontPage_Form1'
+REG_NO = 'regno'
 
-		b.open("http://cbseresults.nic.in/class12/cbse122014_total.htm")
-		b.select_form("FrontPage_Form1")
-		b["regno"]=str(rn)
-		resp=b.submit()
-		cont=resp.read()
-		soup=BeautifulSoup(cont)
-		soup.prettify("utf-8")
-		cont=soup.get_text()
-		cont=cont[cont.index("Roll"):cont.index("Check")]
-		cont=cont.encode("UTF-8").strip() 
+def parse_content(html):
+    """ returns result text from html """
+    soup = BeautifulSoup(html)
+    text = soup.get_text()
+    begin = text.index('Roll')
+    end = text.index('Check')
+    return text[begin:end].encode('UTF-8').strip()
 
-		data.append(cont)
-	return data
+def main(reg_num):
+    """ main function """
+    browser = Browser()
+    browser.open(URL)
+    browser.select_form(FORM)
+    browser[REG_NO] = str(reg_num)
+    response = browser.submit()
+    content = response.read()
+    return parse_content(content)
 
-if __name__ == "__main__":
-	arglen = len(sys.argv)
-	if arglen < 3 or arglen > 4:
-		print "error : usage scrape-cbse [from-roll-nos] [to-roll-nos] [outfile(optional)]"
-	else :
-		a1 = int(sys.argv[1])
-		a2 = int(sys.argv[2])
-		data =  main(a1,a2)
-		if arglen == 4 : 
-			outfile =  sys.argv[3]
-			with open(outfile,'w') as f:
-				f.write('\n ----------------------------------------- \n'.join(data))
-		elif arglen == 3:
-			print '\n ----------------------------------------- \n'.join(data)
-			
-		
-		
+
+if __name__ == '__main__':
+    parser = ArgumentParser(
+        description='gets the cbse results by scraping the website'
+        )
+    parser.add_argument('from_roll_number', type=int)
+    parser.add_argument('to_roll_number', type=int)
+    parser.add_argument('--outfile', type=str, 
+                        help='save output to a file')
+    args = parser.parse_args()
+    results = [main(num) for num in range(args.from_roll_number, args.to_roll_number)]
+    outfile = open(args.outfile, 'w') if args.outfile else stdout
+    for result in results:
+        print >> outfile, result
+            
